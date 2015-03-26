480d0a904e2a28d6d8a8935fbee081e22b935fa1
mannsi <mannsi1983@gmail.com>
2015-03-26 00:10:32 +0000

    Moving log file to home directory, and small refactoring

--- moCo/command.py
+++ moCo/command.py
@@ -1,6 +1,5 @@
 __author__ = 'mannsi'
 
-import ntpath
 import os
 import subprocess
 import moCo.logger as logger
@@ -12,22 +11,24 @@ class Command():
     """
 
     def __init__(self, file_path):
-        self.absolute_file_path = file_path
-        self.basename = ntpath.basename(file_path)
+        self.file_path = file_path
         with open(file_path) as f:
-            self.commands = f.readlines()
+            self.text_commands = f.readlines()
 
     def execute(self):
-        for command in self.commands:
-            logger.get_logger().debug("Executing command '" + command + "'")
-            try:
-                subprocess.call(command.split(), shell=True)
-            except:
-                logger.get_logger().error("Unable to run command '" + command + "'", exc_info=True)
-        try:
+        for text_command in self.text_commands:
+            self._run_command(text_command)
             self._delete_file()
+
+    def _run_command(self, text_command):
+        logger.get_logger().debug("Executing command '" + text_command + "'")
+        try:
+            subprocess.call(text_command.split(), shell=True)
         except:
-            logger.get_logger().error("Unable to delete file '" + self.absolute_file_path + "'", exc_info=True)
+            logger.get_logger().error("Unable to run command '" + text_command + "'", exc_info=True)
 
     def _delete_file(self):
-        os.remove(self.absolute_file_path)
+        try:
+            os.remove(self.file_path)
+        except:
+            logger.get_logger().error("Unable to delete file '" + self.file_path + "'", exc_info=True)
--- moCo/logger.py
+++ moCo/logger.py
@@ -2,12 +2,14 @@ __author__ = 'mannsi'
 
 
 import logging
+import os
 
 
 def initialize_logging(log_level):
     logger = logging.getLogger('moco')
     logger.setLevel(log_level)
-    fh = logging.FileHandler('moco.log')
+
+    fh = logging.FileHandler(os.path.join(os.path.expanduser('~'), 'moco.log'))
     fh.setLevel(log_level)
     ch = logging.StreamHandler()
     ch.setLevel(log_level)
@@ -20,3 +22,4 @@ def initialize_logging(log_level):
 
 def get_logger():
     return logging.getLogger("moco")
+
